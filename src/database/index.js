84404c66ff413122cabb867ee7b84a6d2f3331f2
Pedro <pedro.souza2@estudante.ufla.br>
2022-04-13 01:39:13 -0300

    associations fixed

--- src/database/index.js
+++ src/database/index.js
@@ -1,23 +1,24 @@
 import Sequelize from 'sequelize';
 import dbConfig from '../config/database';
 
+import Comment from '../models/Comment';
+import Post from '../models/Post';
 import Role from '../models/Role';
 import User from '../models/User';
-import UserRole from '../models/UserRole';
-import Post from '../models/Post';
-import Comment from '../models/Comment';
+import UserRoles from '../models/UserRoles';
 
 const connection = new Sequelize(dbConfig[process.env.NODE_ENV]);
 
+Comment.init(connection);
+Post.init(connection);
 Role.init(connection);
 User.init(connection);
-UserRole.init(connection);
-Post.init(connection);
-Comment.init(connection);
+UserRoles.init(connection);
 
+Comment.associate(connection.models);
+Post.associate(connection.models);
 Role.associate(connection.models);
 User.associate(connection.models);
-Post.associate(connection.models);
-Comment.associate(connection.models);
+UserRoles.associate(connection.models);
 
 module.exports = connection;
