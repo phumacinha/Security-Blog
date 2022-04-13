62b6ff09d62227d39c2a931550c1dcce2cbf76fe
Pedro <pedro.souza2@estudante.ufla.br>
2022-04-13 01:40:15 -0300

    routes fixed

--- src/routes.js
+++ src/routes.js
@@ -1,5 +1,7 @@
 import express from 'express';
 
+import ACLChecking from './middlewares/aclChecking';
+
 import RoleController from './controllers/RoleController';
 import UserController from './controllers/UserController';
 // import AdventureTypeController from './controllers/AdventureTypeController';
@@ -9,10 +11,15 @@ import UserController from './controllers/UserController';
 
 const routes = express.Router();
 
+routes.get('/roles', RoleController.index);
 routes.post('/roles', RoleController.store);
-routes.put('/roles', RoleController.update);
+routes.put('/roles', ACLChecking(['creator']), RoleController.update);
 
+routes.get('/users', UserController.index);
+routes.get('/users/:user_id', UserController.user);
+routes.put('/users/:user_id/changePassword', UserController.changePassword);
 routes.post('/users', UserController.store);
+routes.delete('/users/:user_id', UserController.delete);
 
 // routes.get('/aventura', AdventureController.index);
 // routes.post('/users/:user_id/aventura', AdventureController.store);
