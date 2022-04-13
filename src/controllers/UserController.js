c618272d152331e8ac92878500171f45b27c2367
Pedro <pedro.souza2@estudante.ufla.br>
2022-04-13 01:38:58 -0300

    methods fixed and created

--- src/controllers/UserController.js
+++ src/controllers/UserController.js
@@ -5,16 +5,30 @@ import EncryptorService from '../services/EncryptorService';
 
 module.exports = {
   async index(req, res) {
-    const users = await User.findAll();
+    const users = await User.findAll(
+      {
+        include: [
+          {
+            model: Role,
+            as: 'roles',
+            attributes: ['id', 'name', 'identifier'],
+            through: { attributes: [] },
+          },
+        ],
+      },
+      { raw: true },
+    );
+
     return res.json(users);
   },
+
   async store(req, res) {
     const {
       name,
       email,
       login,
       password,
-      rolesId,
+      role_ids,
     } = req.body;
 
     const user = await User.create({
@@ -24,9 +38,84 @@ module.exports = {
       password: EncryptorService.hashPassword(password),
     });
 
-    // const roles = await Promise.all((rolesId || [])
-    //   .map((roleId) => Role.findByPk(roleId).then((role) => role)));
+    role_ids.forEach(async (role_id) => {
+      await user.addRole(role_id);
+    });
+
+    delete user.dataValues.password;
 
     return res.json(user);
+  },
+
+  async user(req, res) {
+    const { user_id } = req.params;
+
+    const user = await User.findByPk(user_id, {
+      include: [
+        {
+          model: Role,
+          as: 'roles',
+          attributes: ['id', 'name', 'identifier'],
+          through: { attributes: [] },
+        },
+      ],
+    });
+
+    if (!user) {
+      return res.status(502).json({ error: 'User not found' });
+    }
+
+    return res.json(user);
+  },
+
+  async changePassword(req, res) {
+    const { user_id } = req.params;
+    const { password, new_password } = req.body;
+
+    const user = await User.findByPk(user_id, {
+      attributes: {
+        include: ['password'],
+      },
+    });
+
+    // TODO check limit time after creation to change password
+
+    if (!user) {
+      return res.status(502).json({ error: 'User not found' });
+    }
+
+    if (user && EncryptorService.comparePassword(password, user)) {
+      const updateResult = await User.update(
+        {
+          password: EncryptorService.hashPassword(new_password),
+          active: true,
+        },
+        {
+          returning: true,
+          where: { id: user_id },
+        },
+      );
+
+      const updatedUser = updateResult[1][0].dataValues;
+      delete updatedUser.password;
+
+      return res.json(updatedUser);
+    }
+
+    return res.status(401).json({ error: 'invalid-credentials' });
+  },
+
+  async delete(req, res) {
+    const { user_id } = req.params;
+
+    const user = await User.findByPk(user_id);
+
+    if (!user) {
+      return res.status(502).json({ error: 'User not found' });
+    }
+
+    await User.destroy({ where: { id: user_id } });
+
+    return res.status(204).json();
   },
 };
