cf9fdce3536336b3e5f268e077f6784ee4091235
Pedro <pedro.souza2@estudante.ufla.br>
2022-04-20 20:36:21 -0300

    emdedded scope created

--- src/models/Comment.js
+++ src/models/Comment.js
@@ -1,15 +1,24 @@
 import { Model, DataTypes } from 'sequelize';
+import User from './User';
 
 class Comment extends Model {
   static init(sequelize) {
     super.init({
       content: DataTypes.TEXT,
       createdAt: DataTypes.DATE,
-    }, { sequelize });
+    }, {
+      scopes: {
+        embedded: {
+          attributes: ['id', 'content', 'createdAt'],
+          include: [{ model: User, as: 'user' }],
+        },
+      },
+      sequelize,
+    });
   }
 
   static associate(models) {
-    this.belongsTo(models.User, { foreignKey: 'user_id', as: 'user' });
+    this.belongsTo(models.User.scope('identification'), { foreignKey: 'user_id', as: 'user' });
     this.belongsTo(models.Post, { foreignKey: 'post_id', as: 'comments' });
   }
 }
